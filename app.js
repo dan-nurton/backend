29d0032c9ba9ebe836fcb49207772d46743a3a4b
Damien Peyrat <da.peyrat@gmail.com>
2022-09-12 16:21:45 +0200

    integration express

---
+++ app.js
@@ -0,0 +1,24 @@
+import express from 'express';
+
+const app = express();
+
+app.use((req, res, next) => {
+    console.log('Requête reçue !');
+    next();
+  });
+  
+app.use((req, res, next) => {
+    res.status(201);
+    next();
+});
+  
+app.use((req, res, next) => {
+    res.json({ message: 'Votre requête a bien été reçue !' });
+    next();
+});
+  
+app.use((req, res, next) => {
+    console.log('Réponse envoyée avec succès !');
+});
+
+export default app;
