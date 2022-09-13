34eddfd5b63fa4564671a25889db9b9ca7f4a314
dan-nurton <da.peyrat@gmail.com>
2022-09-13 09:45:55 +0200

    install bcrypt

--- controllers/user.js
+++ controllers/user.js
@@ -1,28 +1,20 @@
 import User from '../models/User.js';
+import bcrypt from 'bcrypt';
 
 class ControllerUser {
     
-    signup  = (req, res, next) => {
-        const thing = new User({
-          title: req.body.title,
-          description: req.body.description,
-          imageUrl: req.body.imageUrl,
-          price: req.body.price,
-          userId: req.body.userId
-        });
-        thing.save().then(
-          () => {
-            res.status(201).json({
-              message: 'Post saved successfully!'
+    signup = (req, res, next) => {
+        bcrypt.hash(req.body.password, 10)
+          .then(hash => {
+            const user = new User({
+              email: req.body.email,
+              password: hash
             });
-          }
-        ).catch(
-          (error) => {
-            res.status(400).json({
-              error: error
-            });
-          }
-        );
+            user.save()
+              .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
+              .catch(error => res.status(400).json({ error }));
+          })
+          .catch(error => res.status(500).json({ error }));
       };
       
       login = (req, res, next) => {
