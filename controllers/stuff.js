e013435a7a3f5d1a12c857139548d818080f7ba5
dan-nurton <da.peyrat@gmail.com>
2022-09-13 09:18:37 +0200

    Ajout controller

---
+++ controllers/stuff.js
@@ -0,0 +1,103 @@
+import Thing from '../models/Thing.js';
+
+class ControllerStuff {
+    
+    createThing = (req, res, next) => {
+        const thing = new Thing({
+          title: req.body.title,
+          description: req.body.description,
+          imageUrl: req.body.imageUrl,
+          price: req.body.price,
+          userId: req.body.userId
+        });
+        thing.save().then(
+          () => {
+            res.status(201).json({
+              message: 'Post saved successfully!'
+            });
+          }
+        ).catch(
+          (error) => {
+            res.status(400).json({
+              error: error
+            });
+          }
+        );
+      };
+      
+    getOneThing = (req, res, next) => {
+        Thing.findOne({
+          _id: req.params.id
+        }).then(
+          (thing) => {
+            res.status(200).json(thing);
+          }
+        ).catch(
+          (error) => {
+            res.status(404).json({
+              error: error
+            });
+          }
+        );
+      };
+      
+      modifyThing = (req, res, next) => {
+        const thing = new Thing({
+          _id: req.params.id,
+          title: req.body.title,
+          description: req.body.description,
+          imageUrl: req.body.imageUrl,
+          price: req.body.price,
+          userId: req.body.userId
+        });
+        Thing.updateOne({_id: req.params.id}, thing).then(
+          () => {
+            res.status(201).json({
+              message: 'Thing updated successfully!'
+            });
+          }
+        ).catch(
+          (error) => {
+            res.status(400).json({
+              error: error
+            });
+          }
+        );
+      };
+      
+      deleteThing = (req, res, next) => {
+        Thing.deleteOne({_id: req.params.id}).then(
+          () => {
+            res.status(200).json({
+              message: 'Deleted!'
+            });
+          }
+        ).catch(
+          (error) => {
+            res.status(400).json({
+              error: error
+            });
+          }
+        );
+      };
+      
+      getAllStuff = (req, res, next) => {
+        Thing.find().then(
+          (things) => {
+            res.status(200).json(things);
+          }
+        ).catch(
+          (error) => {
+            res.status(400).json({
+              error: error
+            });
+          }
+        );
+      };
+  }
+
+  const stuffCtrl = new ControllerStuff();
+
+
+
+export default stuffCtrl;
---
+++ routes/stuff.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import stuffCtrl from '../controllers/stuff.js';
+const router = express.Router();
+
+router.get('/', stuffCtrl.getAllStuff);
+router.post('/', stuffCtrl.createThing);
+router.get('/:id', stuffCtrl.getOneThing);
+router.put('/:id', stuffCtrl.modifyThing);
+router.delete('/:id', stuffCtrl.deleteThing);
+
+export default router;
