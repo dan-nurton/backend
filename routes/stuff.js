79cbeb638fc7dfec490e39374f7a03bd0c52af7e
Damien Peyrat <da.peyrat@gmail.com>
2022-09-14 11:40:13 +0200

    refactor route/controller

--- routes/stuff.js
+++ routes/stuff.js
@@ -9,33 +9,32 @@ import multer   from '../middleware/multer-config.js';
 //Controller
 import {createThing, getOneThing, modifyThing, deleteThing, getAllStuff} from '../controllers/stuff.js';
 
-
 router.get('/', 
-async (req, res) => {
-    const result = await getAllStuff();
-    if (result.data) {
-        res.status(200).json(result.data);
-    } else {
-        res.status(400).json({
-            error: result.error
-        });
+    async (req, res) => {
+        const result = await getAllStuff();
+        if (result.data) {
+            res.status(200).json(result.data);
+        } else {
+            res.status(400).json({
+                error: result.error
+            });
+        }
     }
-}
 );
 
 router.delete('/:id', auth,
-async (req, res) => {
-    const {id} = req.params;
-    const {userId} = req.auth;
-    const result = await deleteThing(id, userId);
-    if (result.data) {
-        res.status(200).json(result.data);
-    } else {
-        res.status(400).json({
-            error: result.error
-        });
+    async (req, res) => {
+        const {id} = req.params;
+        const {userId} = req.auth;
+        const result = await deleteThing(id, userId);
+        if (result.data) {
+            res.status(200).json(result.data);
+        } else {
+            res.status(400).json({
+                error: result.error
+            });
+        }
     }
-}
 );
 
 router.post('/', auth, multer, createThing);
